cbdaac45ad0b97063758b8bc2e514c098032bc26
somename <someemail>
2017-05-12 20:08:12 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -45,9 +45,10 @@ public class PlanetExplorer {
 		int[] coordinatesOfObstacles=new int[replacer.length];
 		for(int i=0;i<replacer.length;i++){
 			coordinatesOfObstacles[i] = Integer.parseInt(String.valueOf(replacer[i]));
+			System.out.println(coordinatesOfObstacles[i]);
 		}
 		
 		
-		return 12;
+		return coordinatesOfObstacles;
 	}
 }

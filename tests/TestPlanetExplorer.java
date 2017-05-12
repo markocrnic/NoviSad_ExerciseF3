fb1098bec5c4d2097e4f787845f001cd9bf6c5d2
somename <someemail>
2017-05-12 20:16:04 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -13,7 +13,7 @@ public class TestPlanetExplorer {
 	public void test_gettingCoordinatesIntoArrayOfIntegers(){
 		PlanetExplorer ex = new PlanetExplorer(100,100,"(2,3),(3,2)");
 		
-		int[] expected = {2,3,3,2};
-		assertEquals("Cannot get coordinates from obstacles!",expected, ex.getObstacles("(2,3),(3,2)"));
+		String expected = "2332";
+		assertEquals("Cannot get coordinates from obstacles!",expected,);
 	}
 }

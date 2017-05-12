c49ba302f22c93048b11aab6b1774e03a3ce66e0
somename <someemail>
2017-05-12 20:46:24 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -52,10 +52,10 @@ public class TestPlanetExplorer {
 	public void test_ChangingGridSideIfGrideYis100(){
 		PlanetExplorer ex = new PlanetExplorer(100,100,"(2,3),(3,2)");
 		String expected = "0,0,N";
-		for(int i = 0; i<100; i++){
+		for(int i = 0; i<99; i++){
 			ex.executeCommand("f");
 		}
-		assertEquals(expected, )
+		assertEquals(expected,ex.executeCommand("f") );
 	}
 	
 }

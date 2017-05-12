adc27a9c9568a4d6098a74ca05e8966fec35ec8b
somename <someemail>
2017-05-12 20:11:37 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,4 +8,10 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void test_gettingCoordinatesIntoArrayOfIntegers(){
+		PlanetExplorer ex;
+		ex.getObstacles("(2,3),(3,2)");
+	}
 }

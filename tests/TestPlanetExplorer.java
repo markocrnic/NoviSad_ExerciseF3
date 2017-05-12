2383389e874f5f1420c9cae0edeab50f833d85cb
somename <someemail>
2017-05-12 20:51:07 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -51,7 +51,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_ChangingGridSideIfGrideYis100(){
 		PlanetExplorer ex = new PlanetExplorer(100,100,"(2,3),(3,2)");
-		String expected = "0,0,N";
+		String expected = "0, 0, N";
 		for(int i = 0; i<100; i++){
 			ex.executeCommand("f");
 		}
